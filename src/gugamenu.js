6e51be8ab92d255c9043fcd9472aa1e8d27835d1
GugaVL <augusto015123@gmail.com>
2021-02-13 06:40:36 -0300

    First commit

---
+++ src/gugamenu.js
@@ -0,0 +1,12 @@
+const gugamenu = (prefix, pushname) => {
+    return `❍ *Comandos do Guga*
+    
+    ║➩ ❍ ${prefix}setprefix
+    ║➩ ❍ ${prefix}block
+    ║➩ ❍ ${prefix}tm
+    ║➩ ❍ ${prefix}tmctt
+    ║➩ ❍ ${prefix}clearall`
+
+}
+
+exports.gugamenu = gugamenu
---
+++ src/help.js
@@ -0,0 +1,138 @@
+const help = (prefix) => {
+	return `
+「 *Guga* 」
+
+  ❍ *Informações*
+  ❍ Prefix: 「  ${prefix}  」
+  ❍ Dono : Augusto
+  ❍ Número do dono: https://wa.me/+5575998205795
+
+❍ SOBRE
+  
+  ║➩ ❍ ${prefix}info
+  ║➩ ❍ ${prefix}blocklist
+  ║➩ ❍ ${prefix}chatlist
+  ║➩ ❍ ${prefix}ping
+  ║➩ ❍ ${prefix}bugreport
+❍ FAZER
+  
+  ║➩ ❍ ${prefix}figu
+  ║➩ ❍ ${prefix}toimg
+  ║➩ ❍ ${prefix}virarmp3
+  ║➩ ❍ ${prefix}bpink
+  ║➩ ❍ ${prefix}marvellogo
+  ║➩ ❍ ${prefix}snowwrite
+  ║➩ ❍ ${prefix}3dtext
+  ║➩ ❍ ${prefix}ninjalogo
+  ║➩ ❍ ${prefix}water
+  ║➩ ❍ ${prefix}firetext
+  ║➩ ❍ ${prefix}logowolf
+  ║➩ ❍ ${prefix}logowolf2
+  ║➩ ❍ ${prefix}phlogo
+  ║➩ ❍ ${prefix}glitch
+  ║➩ ❍ ${prefix}neonlogo
+  ║➩ ❍ ${prefix}neonlogo2
+  ║➩ ❍ ${prefix}lionlogo
+  ║➩ ❍ ${prefix}jokerlogo
+  ║➩ ❍ ${prefix}shadow
+  ║➩ ❍ ${prefix}burnpaper
+  ║➩ ❍ ${prefix}coffee
+  ║➩ ❍ ${prefix}lovepaper
+  ║➩ ❍ ${prefix}woodblock
+  ║➩ ❍ ${prefix}qowheart
+  ║➩ ❍ ${prefix}mutgrass
+  ║➩ ❍ ${prefix}undergocean
+  ║➩ ❍ ${prefix}woodenboards
+  ║➩ ❍ ${prefix}wolfmetal
+  ║➩ ❍ ${prefix}metalictglow
+  ║➩ ❍ ${prefix}8bit
+  ║➩ ❍ ${prefix}ttp
+  ║➩ ❍ ${prefix}herrypotter
+  ║➩ ❍ ${prefix}pubglogo
+  ║➩ ❍ ${prefix}quotemaker
+  ║➩ ❍ ${prefix}textoft
+  ║➩ ❍ ${prefix}calunia
+  ║➩ ❍ ${prefix}shota
+  ║➩ ❍ ${prefix}dere
+❍ MEDIA
+  
+  ║➩ ❍ ${prefix}trendtwit
+  ║➩ ❍ ${prefix}randomkpop
+  ║➩ ❍ ${prefix}ytsearch
+❍ FALAR COM O BOT
+  
+  ║➩ ❍ ${prefix}chatguga
+  ║➩ ❍ ${prefix}whatday
+  ║➩ ❍ ${prefix}avalie
+  ║➩ ❍ ${prefix}chatpode
+❍ DOWNLOAD
+  
+  ║➩ ❍ ${prefix}images
+  ║➩ ❍ ${prefix}ytmp3
+  ║➩ ❍ ${prefix}ytmp4
+  ║➩ ❍ ${prefix}tiktok
+  ║➩ ❍ ${prefix}joox
+❍ MEME
+  
+  ║➩ ❍ ${prefix}meme
+  ║➩ ❍ ${prefix}memeindo
+❍ SOM
+  
+  ║➩ ❍ ${prefix}musica
+  ║➩ ❍ ${prefix}audio
+❍ MUSICA
+  
+  ║➩ ❍ ${prefix}lirik
+  ║➩ ❍ ${prefix}chord
+❍ STALKEAR
+  
+  ║➩ ❍ ${prefix}tiktokstalk
+  ║➩ ❍ ${prefix}igstalk
+❍ ANIMES
+  
+  ║➩ ❍ ${prefix}neonime
+  ║➩ ❍ ${prefix}pokemon
+  ║➩ ❍ ${prefix}loli
+  ║➩ ❍ ${prefix}waifu
+  ║➩ ❍ ${prefix}randomanime
+  ║➩ ❍ ${prefix}husbu
+  ║➩ ❍ ${prefix}husbu2
+  ║➩ ❍ ${prefix}buscanime
+  ║➩ ❍ ${prefix}nekonime
+❍ ENTRETENIMENTOS
+  
+  ║➩ ❍ ${prefix}alay
+  ║➩ ❍ ${prefix}gantengcek
+  ║➩ ❍ ${prefix}watak
+  ║➩ ❍ ${prefix}hobby
+  ║➩ ❍ ${prefix}game
+  ║➩ ❍ ${prefix}bucin
+  ║➩ ❍ ${prefix}trust
+  ║➩ ❍ ${prefix}dare
+  ║➩ ❍ ${prefix}simi
+❍ COMANDOS DE AUDIO
+  
+  ║➩ ❍ ${prefix}bahasa
+❍ DONO
+  
+  ║➩ ❍ ${prefix}setprefix
+  ║➩ ❍ ${prefix}block
+  ║➩ ❍ ${prefix}bc
+  ║➩ ❍ ${prefix}bcgc
+  ║➩ ❍ ${prefix}clone
+  ║➩ ❍ ${prefix}clearall
+❍ OUTROS
+ 
+  ║➩ ❍ ${prefix}send
+  ║➩ ❍ ${prefix}wame
+  ║➩ ❍ ${prefix}virtex
+  ║➩ ❍ ${prefix}exe
+  ║➩ ❍ ${prefix}qrcode
+  ║➩ ❍ ${prefix}afk
+  ║➩ ❍ ${prefix}timer
+  ║➩ ❍ ${prefix}fml
+  ║➩ ❍ ${prefix}fml2
+`
+}
+
+exports.help = help
